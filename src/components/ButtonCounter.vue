9ce4a8b43485510511e3d91d941dde29f4db3fe3
pandubaguspr <pandubaguspr@gmail.com>
2019-05-09 12:24:05 +0700

    Belajar

---
+++ src/components/ButtonCounter.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>{{ score }}</p>
+        <button v-on:click="decScore()">-</button>
+        <button v-on:click="incScore()">+</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ButtonCounter',
+    data: function () {
+        return{
+            score: 0
+        }
+    },
+    methods: {
+        incScore: function () {
+            this.score++
+        },
+        decScore: function () {
+            this.score--
+        }
+    }
+}
+</script>
